0ce77b4d0a85b162988e9398cb6a7faa6c68c05e
Cesar64Garcia <cesar_garcia7497@hotmail.com>
2018-09-30 19:54:05 -0600

    Con parametros indicando el parámetro que se esta enviando

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-var express = require('express');
-var app = express();
+var http = require('http'),
+    url = require('url'),
+    qs = require('querystring');
+ 
+var server = http.createServer(function (req, res) {
+    var urlParts = url.parse(req.url, true),
+        urlParams = urlParts.query,
+        urlPathname = urlParts.pathname
+        body = '';
+         
+    req.on('data', function (data) {
+        body += data; 
+    });
+ 
+    req.on('end', function () {
+        res.writeHead(200, {'Content-type':'application/json'});
+        if(urlPathname === '/hello/')
+            res.end(JSON.stringify(urlParams));
+        else 
+            res.end("Error 404");
+    });
+ 
+});
+server.listen(3333);
+console.log("Server running at http://localhost:3333");
