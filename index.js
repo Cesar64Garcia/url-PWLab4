7087fb46f5c4ba468bff1cb3f3bca8b710e6d39f
Cesar64Garcia <cesar_garcia7497@hotmail.com>
2018-09-30 20:08:04 -0600

    uri parameters with /

--- index.js
+++ index.js
@@ -3,23 +3,43 @@ var http = require('http'),
     qs = require('querystring');
  
 var server = http.createServer(function (req, res) {
-    var urlParts = url.parse(req.url, true),
-        urlParams = urlParts.query,
+    let urlParts = url.parse(req.url, true),
         urlPathname = urlParts.pathname
-        body = '';
-         
+        body = '',
+        response = '';
+    
+
+    let paths = urlPathname.split('/');
+
     req.on('data', function (data) {
         body += data; 
     });
  
     req.on('end', function () {
         res.writeHead(200, {'Content-type':'application/json'});
-        if(urlPathname === '/hello/')
-            res.end(JSON.stringify(urlParams));
-        else 
-            res.end("Error 404");
+        if(paths.length === 3){
+            if(paths[1] === 'hello'){
+                response = JSON.stringify({hello: paths[2]})
+            } else {
+                response = getJsonErrorResponse(404, 'Page Not Found. Go to the uri localhost:3333/hello/:name')
+            }
+        } else {
+            response = getJsonErrorResponse(404, 'Page Not Found. Go to the uri localhost:3333/hello/:name')
+        }
+        
+        res.end(response);
     });
  
 });
 server.listen(3333);
 console.log("Server running at http://localhost:3333");
+
+
+function getJsonErrorResponse(lngCode, strMessage) {
+    let res = {
+        code: lngCode,
+        message: strMessage
+    }
+
+    return JSON.stringify(res)
+}
